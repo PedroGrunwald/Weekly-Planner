e9794609b9c3db2ea59250ed201a176166b98ea3
PedroGrunwald <pedro.grunwald@gmail.com>
2023-09-19 18:54:35 -0300

    refactor: add functionalities

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable react/prop-types */
 export default function Appointment(props) {
   return (
-    <li>
+    <li className={props.appointment.done ? 'done' : ''}>
       <div>
-        <input type="checkbox"
-         id={`appointment - ${props.appointment.id}`}
-          />
+        <input 
+        type="checkbox"
+        id={`appointment - ${props.appointment.id}`}
+        onChange={() => props.toggleDone(props.appointment.id)}
+        checked={props.appointment.done}
+        />
         <label 
         htmlFor="checkbox"
         >
         {props.appointment.name}
         </label>
       </div>
-      <button>Remover</button>
+      <button onClick={() => props.removeAppointment(props.appointment.id)}>Remover</button>
     </li>
   );
 }
