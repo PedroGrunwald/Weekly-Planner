db2ae935e1fe09b9c082026d345adfe77a93c2f2
PedroGrunwald <pedro.grunwald@gmail.com>
2023-09-19 18:53:44 -0300

    refactor: change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,23 @@
-import { useState } from "react";
-import Appointment from "./components/Appointment";
+import Day from "./components/Day";
+
+const week = [
+  "domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 function App() {
-  const [appointments, setAppointments] = useState([]);
-  const addAppointment = () => {
-    const name = prompt("informe o nome do compromisso:");
-    const newAppointment = {
-      id: Math.floor(1000000 * (Math.random() + 1)),
-      name,
-      done: false,
-    };
-    setAppointments((currentState) => [...currentState, newAppointment]);
-  };
-
-  const toggleDone = (appointmentId) => {
-    setAppointments((currentState) =>
-      currentState.map((appointment) => {
-        if (appointment.id !== appointmentId) return appointment;
-        else return { ...appointment, done: !appointment.done };
-      })
-    );
-  };
-
-  const removeAppointment = (appointmentId) => {
-    setAppointments((currentState) =>
-      currentState.filter((appointment) => appointment.id !== appointmentId)
-    );
-  };
-
   return (
     <main>
       <h1>Planner Semanal</h1>
-      <div className="day">
-        <h2>Segunda-Feira</h2>
-        <ul>
-          {appointments.map((appointment) => (
-            <Appointment key={Appointment.id} appointment={appointment} />
-          ))}
-        </ul>
-        <button onClick={addAppointment}>Adicionar</button>
+      <div className="days">
+        {week.map((day) => (
+          <Day key={day} day={day} />
+        ))}
       </div>
     </main>
   );
