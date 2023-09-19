fe9d185298d4711b9280f061bf87cb9d2c3694e3
PedroGrunwald <pedro.grunwald@gmail.com>
2023-09-19 18:08:41 -0300

    fix: Appointment component

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -1,11 +1,18 @@
-export default function Appointment(props){
-    return (
-        <li>
-            <div>
-                <input type= "checkbox" id="checkbox" />
-                <label htmlFor="checkbox">{props.appointment.name}</label>
-            </div>
-            <button>Remover</button>
-        </li>
-    )
+/* eslint-disable react/prop-types */
+export default function Appointment(props) {
+  return (
+    <li>
+      <div>
+        <input type="checkbox"
+         id={`appointment - ${props.appointment.id}`}
+          />
+        <label 
+        htmlFor="checkbox"
+        >
+        {props.appointment.name}
+        </label>
+      </div>
+      <button>Remover</button>
+    </li>
+  );
 }
