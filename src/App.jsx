e3dc6ccfe87053b657899df466b9b29b37ee0117
PedroGrunwald <pedro.grunwald@gmail.com>
2023-09-19 15:29:37 -0300

    feat: add appointment and  scheleton application

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [appointments, setAppointments] = useState([]);
+  const addAppointment = () => {
+    const name = prompt("informe o nome do compromisso:");
+    const newAppointment = {
+      id: Math.floor(1000000 * (Math.random() + 1)),
+      name,
+      done: false,
+    };
+    setAppointments([...appointments, newAppointment])
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <h1>Planner Semanal</h1>
+      <div className="day">
+        <h2>Segunda-Feira</h2>
+        <ul>
+          {appointments.map((appointment) => (
+            <li key={appointment.id}>{appointment.name}</li>
+          ))}
+        </ul>
+        <button onClick={addAppointment}>Adicionar</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
